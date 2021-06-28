b2fc9b55a864038c2c4620422384c1b4fa5ce325
Antonio Vázquez <julianantonio.v@hotmail.com>
2021-06-28 14:20:20 -0300

    modified css media queries

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,12 @@ const Footer = ()=>{
     return(
       <footer>
       <h4>© 2021 Toni. All rights reserved</h4>
-    <a href='https://github.com/ToniVaz'><span className="icon-github"></span></a>
+      <div className='container-a'>
+      <a href='https://github.com/ToniVaz'><span className="icon-github"></span></a>
     <a href='https://www.linkedin.com/in/antonio-vazquez-tony/'><span className="icon-linkedin"></span></a>
     <a href='https://wa.me/541124854134/?text=Hola%20como%20va?.%20Me%20interesa%20contactarte'><span className="icon-whatsapp"></span></a>
-  </footer>
+      </div>
+   </footer>
     )
   }
 
