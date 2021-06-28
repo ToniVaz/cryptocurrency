37ed772c2427751cb08d180f8a99f787e16e2eee
Antonio Vázquez <julianantonio.v@hotmail.com>
2021-06-28 12:33:31 -0300

    first commit for crypto-website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ const Footer = ()=>{
     return(
       <footer>
       <h4>© 2021 Toni. All rights reserved</h4>
-    <a href='https://github.com/ToniVaz'><span class="icon-github"></span></a>
-    <a href='https://www.linkedin.com/in/antonio-vazquez-tony/'><span class="icon-linkedin"></span></a>
-    <a href='https://wa.me/541124854134/?text=Hola%20como%20va?.%20Me%20interesa%20contactarte'><span class="icon-whatsapp"></span></a>
+    <a href='https://github.com/ToniVaz'><span className="icon-github"></span></a>
+    <a href='https://www.linkedin.com/in/antonio-vazquez-tony/'><span className="icon-linkedin"></span></a>
+    <a href='https://wa.me/541124854134/?text=Hola%20como%20va?.%20Me%20interesa%20contactarte'><span className="icon-whatsapp"></span></a>
   </footer>
     )
   }
